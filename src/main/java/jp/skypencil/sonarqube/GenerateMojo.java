9ebfce51cc4d2934aae917d1027ae846922739ec
Kengo TODA <skypencil@gmail.com>
2017-06-14 21:51:53 +0800

    initial commit
    almost copied from https://github.com/KengoTODA/findbugs-slf4j/blob/findbugs-slf4j-1.2.4/sonar-plugin/src/main/java/jp/skypencil/findbugs/slf4j/sonar/RuleXmlGenerator.java

---
+++ src/main/java/jp/skypencil/sonarqube/GenerateMojo.java
@@ -0,0 +1,50 @@
+package jp.skypencil.sonarqube;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+@Mojo(name = "generate", threadSafe = true, defaultPhase = LifecyclePhase.GENERATE_RESOURCES)
+public class GenerateMojo extends AbstractMojo {
+    @Parameter(required = true, defaultValue = "${project.basedir}/src/main/resources/findbugs.xml")
+    private File findbugs;
+
+    @Parameter(required = true, defaultValue = "${project.basedir}/src/main/resources/messages.xml")
+    private File messages;
+
+    @Parameter(required = true, defaultValue = "${project.build.outputDirectory}/rule.xml")
+    private File output;
+
+    @Parameter(required = true, defaultValue = "false")
+    private boolean skip;
+
+    @Override
+    public void execute() throws MojoExecutionException {
+        if (skip) {
+            getLog().info("Skip generating rule XML for SonarQube");
+            return;
+        }
+
+        if (!findbugs.isFile()) {
+            throw new MojoExecutionException("findbug.xml not found at " + findbugs.getAbsolutePath());
+        }
+        if (!messages.isFile()) {
+            throw new MojoExecutionException("messages.xml not found at " + messages.getAbsolutePath());
+        }
+        if (!output.getParentFile().mkdirs()) {
+            throw new MojoExecutionException("failed to make directory at " + output.getParent());
+        }
+
+        try {
+            getLog().info("Generating rule XML for SonarQube at " + output.getAbsolutePath());
+            new RuleXmlGenerator().generate(findbugs, messages, output);
+        } catch (IOException e) {
+            throw new MojoExecutionException("I/O problem occurred", e);
+        }
+    }
+}
---
+++ src/main/java/jp/skypencil/sonarqube/RuleXmlGenerator.java
@@ -0,0 +1,99 @@
+package jp.skypencil.sonarqube;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Node;
+import org.dom4j.io.SAXReader;
+
+import com.google.common.base.Charsets;
+import com.google.common.base.Objects;
+import com.google.common.io.Closeables;
+import com.google.common.io.Files;
+
+/**
+ * <p>Generate rule.xml for Sonar (SonarQube).</p>
+ */
+public class RuleXmlGenerator {
+    void generate(File findbugsFile, File messageFile, File output) throws IOException {
+        SAXReader reader = new SAXReader();
+        try {
+            Document message = reader.read(messageFile);
+            Document findbugs = reader.read(findbugsFile);
+
+            @SuppressWarnings("unchecked")
+            List<Node> bugPatterns = message.selectNodes("/MessageCollection/BugPattern");
+            @SuppressWarnings("unchecked")
+            List<Node> findbugsAbstract = findbugs.selectNodes("/FindbugsPlugin/BugPattern");
+
+            writePatterns(findbugsAbstract, bugPatterns, output);
+        } catch (DocumentException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    private void writePatterns(List<Node> findbugsAbstract, List<Node> bugPatterns, File output) throws IOException {
+        BufferedWriter writer = Files.newWriter(output, Charsets.UTF_8);
+        boolean threw = true;
+        try {
+            writer.write("<rules>\n");
+            for (Node bugPattern : bugPatterns) {
+                writeBugPattern(bugPattern, findbugsAbstract, writer);
+            }
+            writer.write("</rules>\n");
+            threw = false;
+        } finally {
+            Closeables.close(writer, threw);
+        }
+    }
+
+    private void writeBugPattern(Node bugPattern, List<Node> findbugsAbstract, BufferedWriter writer) throws IOException {
+        String type = bugPattern.valueOf("@type");
+        String description = bugPattern.selectSingleNode("ShortDescription").getText();
+
+        Node data = findByType(findbugsAbstract, type);
+        String abbrev = data.valueOf("@abbrev");
+        String category = data.valueOf("@category");
+        String priority = decidePriority(category);
+
+        String line = String.format("  <rule key=\"%s\">\n" +
+                "    <priority>%s</priority>\n" +
+                "    <name><![CDATA[%s - %s]]></name>\n" +
+                "    <description><![CDATA[[%s] %s]]></description>\n" +
+                "    <configKey><![CDATA[%s]]></configKey>\n" +
+                "    <tag>%s</tag>\n" +
+                "  </rule>\n",
+                type, priority, category, description, abbrev, description, type, category.toLowerCase().replace("_","-"));
+        writer.write(line);
+    }
+
+    private String decidePriority(String category) {
+        if (category == null) {
+            return "MAJOR";
+        }
+
+        switch (category) {
+        case "STYLE":
+        case "MALICIOUS_CODE":
+        case "I18N":
+        case "EXPERIMENTAL":
+            return "INFO";
+        default:
+            return "MAJOR";
+        }
+    }
+
+    private Node findByType(List<Node> findbugsAbstract, String type) {
+        for (Node node : findbugsAbstract) {
+            if (Objects.equal(type, node.valueOf("@type"))) {
+                return node;
+            }
+        }
+        throw new IllegalArgumentException("cannot find " + type);
+    }
+
+}
